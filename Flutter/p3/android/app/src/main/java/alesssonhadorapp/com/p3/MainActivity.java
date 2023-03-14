a0f9e3c19e406ce8790811decc125b59d96a793f
Suedz01 <suedalex2@hotmail.com>
2023-03-14 00:22:15 -0300

    Atualizacao

--- Flutter/p3/android/app/src/main/java/alesssonhadorapp/com/p3/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package alesssonhadorapp.com.p3;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ LandingPages/SecondProject/script.js
@@ -0,0 +1,10 @@
+// qeuerySelector is returning null because it executes
+//  before the DOM fully loads.
+window.onload=function(){ 
+  const menu = document.querySelector('.menu');
+  const NavMenu = document.querySelector('.nav-menu');
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+  })
+}
